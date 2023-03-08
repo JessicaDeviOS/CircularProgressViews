530949fc4c4099dedcf736cf86dd58b23235d88d
Jessica Mallian <jlmallian@gmail.com>
2023-03-07 20:20:03 -0600

    refactoring

--- CircularProgressViews/CircularProgressViewWithBackground.swift
+++ CircularProgressViews/CircularProgressViewWithBackground.swift
@@ -9,19 +9,19 @@ import SwiftUI
 
 struct CircularProgressViewWithBackground: View {
     let progress: Double
-    let cappedProgressBar: Bool
-    let color: Color
+    let shouldHaveRoundLineCap: Bool
+    let progressBarColor: Color
     let lineWidth: CGFloat
-    let opacity: Double
+    let progressBackgroundOpacity: Double
     let insideColor: Color
     let width: CGFloat
     
     init(progress: Double, cappedProgressBar: Bool = true, color: Color, lineWidth: CGFloat = 10.0, opacity: Double = 0.5, insideColor: Color, width: CGFloat = 50.0) {
         self.progress = progress
-        self.cappedProgressBar = cappedProgressBar
-        self.color = color
+        self.shouldHaveRoundLineCap = cappedProgressBar
+        self.progressBarColor = color
         self.lineWidth = lineWidth
-        self.opacity = opacity
+        self.progressBackgroundOpacity = opacity
         self.insideColor = insideColor
         self.width = width
     }
@@ -32,11 +32,11 @@ struct CircularProgressViewWithBackground: View {
                 .foregroundColor(insideColor)
                 .frame(width: width, height: width)
             Circle()
-                .stroke(color.opacity(opacity), style: StrokeStyle(lineWidth: lineWidth))
+                .stroke(progressBarColor.opacity(progressBackgroundOpacity), style: StrokeStyle(lineWidth: lineWidth))
                 .frame(width: width + lineWidth, height: width + lineWidth)
             Circle()
                 .trim(from: 0, to: progress)
-                .stroke(color, style: StrokeStyle(lineWidth: lineWidth, lineCap: cappedProgressBar ? .round : .square))
+                .stroke(progressBarColor, style: StrokeStyle(lineWidth: lineWidth, lineCap: shouldHaveRoundLineCap ? .round : .square))
                 .frame(width: width + lineWidth, height: width + lineWidth)
                 .rotationEffect(.degrees(-90))
                 .animation(.easeOut, value: progress)
