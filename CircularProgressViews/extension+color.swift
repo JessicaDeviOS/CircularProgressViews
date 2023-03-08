84adf7e3ad8cb32a82ad9717c232f515174b862f
Jessica Mallian <jlmallian@gmail.com>
2023-03-07 20:31:30 -0600

    added some colors found with coolors dot co

---
+++ CircularProgressViews/extension+color.swift
@@ -0,0 +1,16 @@
+//
+//  extension+color.swift
+//  CircularProgressViews
+//
+//  Created by Jessica Mallian on 3/7/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let mindaro = Color("mindaro")
+    static let darkPurple = Color("darkPurple")
+    static let feldgrau = Color("feldgrau")
+    static let coolGray = Color("coolGray")
+    static let bringPink = Color("brightPink")
+}
