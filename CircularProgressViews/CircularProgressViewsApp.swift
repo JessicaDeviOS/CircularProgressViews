7381bbd04317b77ba6e3f3dfefbfa08663b5ffb3
Jessica Mallian <jlmallian@gmail.com>
2023-03-07 20:04:36 -0600

    created project

---
+++ CircularProgressViews/CircularProgressViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProgressViewsApp.swift
+//  CircularProgressViews
+//
+//  Created by Jessica Mallian on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CircularProgressViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
